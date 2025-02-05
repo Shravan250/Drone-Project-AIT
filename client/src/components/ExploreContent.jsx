751a17ae92eb5ad11423a7bd698ec656eb04fabd
Shravan250 <shravanbobade25@gmail.com>
2025-02-05 20:42:32 +0530

    added text clipping

--- client/src/components/ExploreContent.jsx
+++ client/src/components/ExploreContent.jsx
@@ -6,9 +6,10 @@ const ExploreContent = () => {
   return (
     <div className="h-full w-full flex flex-col justify-center items-center">
       <Container className="m-3 mt-5 flex flex-col gap-2">
-        <h1 className="text-4xl font-bold text-center">
-          Welcome to explore section !
+        <h1 className="text-4xl font-bold text-center bg-gradient-to-r from-blue-400 via-purple-500 to-blue-700 bg-clip-text text-transparent">
+          Welcome to explore section!
         </h1>
+
         <div>
           <CardGrid />
         </div>
